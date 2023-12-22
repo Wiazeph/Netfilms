ed2d181f28e6a4d074a3c09d38a057de1e42933d
Emre Erden <emreerden@pm.me>
2023-12-23 02:24:22 +0300

    create main layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/assets/css/index.css'
+import Header from '@/layouts/main/header'
+import Footer from '@/layouts/main/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        </body>
     </html>
   )
 }
